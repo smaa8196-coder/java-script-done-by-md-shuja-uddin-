5c67dd9b45677befe4803c947d0f8cccef4dbb59
smaa8196-coder <smaa8196@gmail.com>
2026-09-05 17:07:41 +0530

    Add files via upload

---
+++ project12/project12.js
@@ -0,0 +1,44 @@
+let [seconds,minutes,hours]=[0,0,0];
+let display = document.getElementById("display");
+let timer = null;
+
+function stopwatch(){
+    seconds++;
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++;
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes <10 ? "0" + minutes :minutes;
+    let s = seconds <10 ? "0" + seconds :seconds;
+
+
+    display.innerHTML = h+":"+m+":"+s;
+}
+
+function watchstop(){
+    if(timer!== null)
+    {
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch,1000);
+}
+
+function watchstart(){
+        clearInterval(timer);
+}
+
+function watchreset(){
+    clearInterval(timer);
+    timer = null;
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    display.innerHTML="00:00:00";
+
+}
+
